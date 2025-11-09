7acf01e296c73bd8e03fffaed23492b904ebf659
gustavhedegard <gustav.selin@gmail.com>
2025-11-09 13:37:04 +0100

    add mapcontrollers

--- Controllers/MatchController.cs
+++ Controllers/MatchController.cs
@@ -4,12 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class MatchController : ControllerBase
 {
-    private readonly MatchService _matchService;
-
-    public MatchController(MatchService matchService)
-    {
-        _matchService = matchService;
-    }
 
     [HttpGet("get-score")]
     public IActionResult GetScore()
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -13,5 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
